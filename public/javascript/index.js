fe9512b0ccd445ca47e8db8d27e930127f45b57b
Adenildo Junior <adenildojunior52@gmail.com>
2021-05-24 22:47:39 -0300

    :rocket: deploy: Update game url

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,7 @@
-const STABLE_VERSION = 'https://script.google.com/macros/s/AKfycbyLoPEIhYu95I8UVIeq2fHBnOg-vD96Lit_jDlSiw54FmTQuFdX5wE43XyzO7Ss229Z/exec';
-
+//const STABLE_VERSION = 'https://script.google.com/macros/s/AKfycbyLoPEIhYu95I8UVIeq2fHBnOg-vD96Lit_jDlSiw54FmTQuFdX5wE43XyzO7Ss229Z/exec';
+const STABLE_VERSION = 'https://script.google.com/macros/s/AKfycbyfV9DWqt0RA1UC2S_P-Sm9CnlQb1rZj_6R2vGEacuR1eyNHZQRLLdsVS5LvuukFYdmcQ/exec'
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
-console.log(urlParams.get('u'))
 
 function newGame(st) {
     window.location.href = `${STABLE_VERSION}?/u=${urlParams.get('u')}&st=${st}`
