e227843e891f8cbbf273f21b86aa15a65a4e7788
Adenildo Junior <adenildojunior52@gmail.com>
2021-05-24 22:44:41 -0300

    :building_construction: feat: Set session type

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,3 +1,9 @@
+const STABLE_VERSION = 'https://script.google.com/macros/s/AKfycbyLoPEIhYu95I8UVIeq2fHBnOg-vD96Lit_jDlSiw54FmTQuFdX5wE43XyzO7Ss229Z/exec';
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
-console.log(urlParams.get('user'))
+console.log(urlParams.get('u'))
+
+function newGame(st) {
+    window.location.href = `${STABLE_VERSION}?/u=${urlParams.get('u')}&st=${st}`
+}
