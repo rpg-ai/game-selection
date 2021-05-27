4c72b575ecde550a6a9355fddca3628d0c7d887f
Adenildo Junior <adenildojunior52@gmail.com>
2021-05-26 22:37:54 -0300

    :wrench: fix: Updates fetch

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,22 +3,11 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
 
 async function check_game() {
-    const params = {
-        u: urlParams.get('u'),
-    };
-    const options = {
-        method: 'POST',
-        mode: 'no-cors',
-        body: JSON.stringify( params )
-    };
-    const request = await fetch(STABLE_VERSION, options)
-        .then(response => {
-            console.log(response)
-            if (response["hasGame"]) {
-                console.log('Entrei no hasGame')
-                window.location.href = `${STABLE_VERSION}?u=${urlParams.get('u')}`
-            }
-        })
+    let request = await fetch(`${STABLE_VERSION}?u=${urlParams.get('u')}`)
+
+    if (request['hasGame']) {
+        window.location.href = `${STABLE_VERSION}?u=${urlParams.get('u')}`
+    }
 }
 
 check_game()
