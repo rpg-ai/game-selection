e3d96dd60c656c018e2f0f8d9f5bb40cc7357bfc
Adenildo Junior <adenildojunior52@gmail.com>
2021-05-26 22:43:26 -0300

    :wrench: fix: Updates async

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,16 +2,18 @@ const STABLE_VERSION = 'https://script.google.com/macros/s/AKfycbxYPo01qUwYPvt3K
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
 
-async function check_game() {
-    let request = await fetch(`${STABLE_VERSION}?u=${urlParams.get('u')}`,
+function check_game() {
+    fetch(`${STABLE_VERSION}?u=${urlParams.get('u')}`,
         {
             method: 'POST',
             mode: 'no-cors'
-        });
-
-    if (request['hasGame']) {
-        window.location.href = `${STABLE_VERSION}?u=${urlParams.get('u')}`
-    }
+    }).then(
+        request => {
+            if (request['hasGame']) {
+                window.location.href = `${STABLE_VERSION}?u=${urlParams.get('u')}`
+            }
+        }
+    );
 }
 
 check_game();
